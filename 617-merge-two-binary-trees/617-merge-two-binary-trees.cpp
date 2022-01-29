122490715bd9cc2eb1d802da251970ba392bd9a9
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-01-29 18:13:50 +0530

    Time: 53 ms (40.56%), Space: 32.3 MB (87.20%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+
+    if (t1 && t2) { 
+        t1->val+=t2->val;
+        t1->left = mergeTrees(t1->left,t2->left);  
+        t1->right = mergeTrees(t1->right, t2->right);
+    } else { 
+	    return t1 ? t1 : t2;  
+	}
+    
+    return t1; 
+}
+};
