e6e26ad6d56cb546a00f76036035a9593b033ac9
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-01-31 11:58:56 +0530

    Time: 15 ms (14.77%), Space: 8.5 MB (88.39%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,33 @@
+static auto fast_io = []
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& arr) {
+        int ans=INT_MAX;
+        if(arr.size()==1){
+            return arr[0][0];
+        }
+        for(int i=1;i<arr.size();i++){
+            for(int j=0;j<arr[i].size();j++){
+                if(j==0){
+                    arr[i][j]=arr[i-1][j]+arr[i][j];
+                }
+                else if(j==arr[i].size()-1){
+                    arr[i][j]=arr[i][j]+arr[i-1][j-1];
+                }
+                else{
+                    arr[i][j]=arr[i][j]+min(arr[i-1][j],arr[i-1][j-1]);
+                }
+                if(i==arr.size()-1){
+                    ans=min(ans,arr[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
