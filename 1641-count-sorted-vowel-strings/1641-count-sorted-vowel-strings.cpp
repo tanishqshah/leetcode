6213468186b1e687bf6e9b99dd588ef429848685
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-02-02 14:47:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.37%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,33 +1,8 @@
-static auto fast_io = []
-{
-	ios::sync_with_stdio(false);
-	cout.tie(nullptr);
-	cin.tie(nullptr);
-	return 0;
-}();
+
 
 class Solution {
 public:
-    void get(vector<vector<int>>&ans , vector<int> &str , int ind, int n){
-        if(str.size()==n){
-            ans.push_back(str);
-            return ;
-        }
-        for(int i= ind ;i<5;i++){
-            str.push_back(i);
-            get(ans,str,i,n);
-            str.pop_back();
-        }
-        
-    }
     int countVowelStrings(int n) {
-        if(n==50){
-            return 316251;
-        }
-        vector<int>str;
-        int i=0;
-        vector<vector<int>>ans;
-        get(ans,str,i,n);
-        return ans.size();
+        return (n+1)*(n+2)*(n+3)*(n+4)/24;
     }
 };
