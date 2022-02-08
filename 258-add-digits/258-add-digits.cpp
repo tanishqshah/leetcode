548cf6363cf1f6d0a07a963b95bd426dfb27f64c
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-02-08 13:36:25 +0530

    Time: 7 ms (11.31%), Space: 5.9 MB (75.31%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num>=0 && num<=9)
+            return num;
+        else if(num%9==0)
+            return 9;
+        return num%9;
+    }
+};
