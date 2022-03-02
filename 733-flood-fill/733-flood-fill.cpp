2db9139636cdda1bf2504237e145f2fed736ca16
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-03-02 22:18:01 +0530

    Time: 11 ms (70.57%), Space: 13.9 MB (80.53%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(newColor==image[sr][sc])
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int r, int c, int n) {
+        if(n==image[r][c]){
             return image;
-        change(image,sr,sc,newColor , image[sr][sc]);
+        }
+        change(image,r,c,n,image[r][c]);
         return image;
     }
-    
-    void change(vector<vector<int>>&image , int r, int c, int color, int oldcolor){
-        if(r>=image.size() || r<0 || c<0 || c>=image[0].size() || image[r][c]!=oldcolor)
+    void change(vector<vector<int>>&image , int r, int c, int n, int old){
+        if(r>=image.size() || r<0 || c<0 || c>=image[0].size() || image[r][c]!=old){
             return;
-        
-        image[r][c]=color;
-        change(image,r-1,c,color,oldcolor);
-        change(image,r,c-1,color,oldcolor);
-        change(image,r+1,c,color,oldcolor);
-        change(image,r,c+1,color,oldcolor);
-        
+        }
+        image[r][c]=n;
+        change(image,r-1,c,n,old);        
+        change(image,r+1,c,n,old);
+        change(image,r,c+1,n,old);
+        change(image,r,c-1,n,old);
+
     }
 };
