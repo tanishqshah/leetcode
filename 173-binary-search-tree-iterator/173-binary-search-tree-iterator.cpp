2e6c5d3f7c43c31a6321f3049d8898a6cb1172a3
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-04-20 10:09:43 +0530

    Time: 36 ms (59.40%), Space: 24.2 MB (56.50%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -0,0 +1,36 @@
+class BSTIterator {
+private:
+    stack<TreeNode*> st;
+public:
+    BSTIterator(TreeNode *root) {
+        find_left(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        if (st.empty())
+            return false;
+        return true;
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* top = st.top();
+        st.pop();
+        if (top->right != NULL)
+            find_left(top->right);
+            
+        return top->val;
+    }
+    
+    /** put all the left child() of root */
+    void find_left(TreeNode* root)
+    {
+        TreeNode* p = root;
+        while (p != NULL)
+        {
+            st.push(p);
+            p = p->left;
+        }
+    }
+};
