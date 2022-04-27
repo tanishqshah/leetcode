673d62de39a62f3696bd643cb4c3c26fd4ff4efc
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-04-27 07:41:32 +0530

    Time: 141 ms (95.68%), Space: 49 MB (82.54%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int find(vector<int>& ds, int i) {
+  return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
+}
+string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+  vector<int> ds(s.size(), -1);
+  vector<vector<int>> m(s.size());
+  for (auto& p : pairs) {
+    auto i = find(ds, p[0]), j = find(ds, p[1]);
+    if (i != j) {
+        if (-ds[i] < -ds[j]) 
+            swap(i, j);
+        ds[i] += ds[j];
+        ds[j] = i;
+    }
+  }
+  for (auto i = 0; i < s.size(); ++i) 
+      m[find(ds, i)].push_back(i);
+  for (auto &ids : m) {
+    string ss = "";
+    for (auto id : ids) 
+        ss += s[id];
+    sort(begin(ss), end(ss));
+    for (auto i = 0; i < ids.size(); ++i) 
+        s[ids[i]] = ss[i];
+  }
+  return s;
+}
+};
