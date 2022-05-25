a730917b09567cb7456e0b3a80607e7e53e50719
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-05-25 09:40:31 +0530

    Time: 11 ms (70.02%), Space: 14 MB (50.93%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -7,15 +7,35 @@ public:
         change(image,r,c,n,image[r][c]);
         return image;
     }
-    void change(vector<vector<int>>&image , int r, int c, int n, int old){
+    void change(vector<vector<int>>&image,int r,int c,int n,int old){
         if(r>=image.size() || r<0 || c<0 || c>=image[0].size() || image[r][c]!=old){
             return;
         }
         image[r][c]=n;
-        change(image,r-1,c,n,old);        
+        // change(image,r-1,c,n,old);
+        change(image,r-1,c,n,old);
         change(image,r+1,c,n,old);
-        change(image,r,c+1,n,old);
         change(image,r,c-1,n,old);
-
+        change(image,r,c+1,n,old);
+        
     }
 };
+
+// vector<vector<int>> floodFill(vector<vector<int>>& image, int r, int c, int n) {
+//         if(n==image[r][c]){
+//             return image;
+//         }
+//         change(image,r,c,n,image[r][c]);
+//         return image;
+//     }
+//     void change(vector<vector<int>>&image , int r, int c, int n, int old){
+//         if(r>=image.size() || r<0 || c<0 || c>=image[0].size() || image[r][c]!=old){
+//             return;
+//         }
+//         image[r][c]=n;
+//         change(image,r-1,c,n,old);        
+//         change(image,r+1,c,n,old);
+//         change(image,r,c+1,n,old);
+//         change(image,r,c-1,n,old);
+
+//     }
