34d68738875b34fe641c34cf5120178e093b01f4
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-06-08 23:49:37 +0530

    Added solution - LeetHub

--- Construct Tree from Inorder & Preorder - GFG/construct-tree-from-inorder-preorder.cpp
+++ Construct Tree from Inorder & Preorder - GFG/construct-tree-from-inorder-preorder.cpp
@@ -41,24 +41,22 @@ struct Node
 */
 class Solution{
     public:
-    int findpos(int in[],int ele,int n){
+    void create(int in[],map<int,int>&mp,int n){
         for(int i=0;i<n;i++){
-            if(in[i]==ele)
-                return i;
+            mp[in[i]]=i;
         }
-        return -1;
     }
     
-    Node* solve(int in[],int pre[],int &index,int inst,int inend,int n){
+    Node* solve(int in[],int pre[],int &index,int inst,int inend,int n,map<int,int>&mp){
         if(index>= n || inst>inend)
             return NULL;
         int ele=pre[index++];
         Node* root=new Node(ele);
         
-        int pos=findpos(in,ele,n);
+        int pos=mp[ele];
         
-        root->left=solve(in,pre,index,inst,pos-1,n);
-        root->right=solve(in,pre,index,pos+1,inend,n);
+        root->left=solve(in,pre,index,inst,pos-1,n,mp);
+        root->right=solve(in,pre,index,pos+1,inend,n,mp);
         
         return root;
         
@@ -67,7 +65,9 @@ class Solution{
     Node* buildTree(int in[],int pre[], int n)
     {
         int preindex=0;
-        Node* ans=solve(in,pre,preindex,0,n-1,n);
+        map<int,int>mp;
+        create(in,mp,n);
+        Node* ans=solve(in,pre,preindex,0,n-1,n,mp);
         return ans;
     }
 };
