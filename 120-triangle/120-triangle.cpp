bc71d80e239e390ba7d38c01cde78f66b1353990
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-06-13 14:31:39 +0530

    Time: 7 ms (75.60%), Space: 8.6 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,10 +1,3 @@
-static auto fast_io = []
-{
-	ios::sync_with_stdio(false);
-	cout.tie(nullptr);
-	cin.tie(nullptr);
-	return 0;
-}();
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& arr) {
