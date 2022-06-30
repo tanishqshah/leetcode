b40f232b54cff7a7e756a357a653f14bcb6ebcdb
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-07-01 03:34:33 +0530

    Time: 12 ms (81.10%), Space: 10.9 MB (59.76%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size(), steps = 0;
         sort(nums.begin(), nums.end());
         for(int i=0; i<n/2; i++){
-            steps += nums[n-1-i] - nums[i]; 
+            steps = steps + (nums[n-1-i] - nums[i]); 
         }
         return steps;
     }
