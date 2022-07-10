fe05748819be5b246c67d0671d505a1c25a8d1f1
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-07-10 23:07:19 +0530

    Time: 478 ms (8.57%), Space: 74.1 MB (28.68%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -1,26 +1,69 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head) return head;
-        if(head->next==NULL) return head;
-        multiset<pair<int,ListNode*>> m;
-        while(head!=NULL){
-            m.insert({head->val,head});
-            head=head->next;
+        //If List Contain a Single or 0 Node
+        if(head == NULL || head ->next == NULL)
+            return head;
+        
+        
+        ListNode *temp = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        // 2 pointer appraoach / turtle-hare Algorithm (Finding the middle element)
+        while(fast !=  NULL && fast -> next != NULL)
+        {
+            temp = slow;
+            slow = slow->next;          //slow increment by 1
+            fast = fast ->next ->next;  //fast incremented by 2
+            
+        }   
+        temp -> next = NULL;            //end of first left half
+        
+        ListNode* l1 = sortList(head);    //left half recursive call
+        ListNode* l2 = sortList(slow);    //right half recursive call
+        
+        return mergelist(l1, l2);         //mergelist Function call
+            
+    }
+    
+    //MergeSort Function O(n*logn)
+    ListNode* mergelist(ListNode *l1, ListNode *l2)
+    {
+        ListNode *ptr = new ListNode(0);
+        ListNode *curr = ptr;
+        
+        while(l1 != NULL && l2 != NULL)
+        {
+            if(l1->val <= l2->val)
+            {
+                curr -> next = l1;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                curr -> next = l2;
+                l2 = l2 -> next;
+            }
+        
+        curr = curr ->next;
+        
         }
-        pair<int,ListNode*> p=*m.begin();
-        head=p.second;
-        ListNode *root=head;
-        m.erase(m.begin());
-        while(m.size()>1){
-            pair<int,ListNode*> k=*m.begin();
-            root->next=k.second;
-            root=root->next;
-            m.erase(m.begin());
+        
+        //for unqual length linked list
+        
+        if(l1 != NULL)
+        {
+            curr -> next = l1;
+            l1 = l1->next;
         }
-        pair<int,ListNode*> k=*m.begin();
-        root->next=k.second;
-        root->next->next=NULL;
-        return head;
+        
+        if(l2 != NULL)
+        {
+            curr -> next = l2;
+            l2 = l2 ->next;
+        }
+        
+        return ptr->next;
     }
 };
