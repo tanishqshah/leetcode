b523d8c1d8d6fed6ff67d60c921f835994a9f48a
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-07-21 01:05:08 +0530

    Time: 194 ms (91.20%), Space: 38 MB (85.02%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    int numMatchingSubseq(string S, vector<string>& words) {
-    vector<const char*> waiting[128];
-    for (auto &w : words)
-        waiting[w[0]].push_back(w.c_str());
-    for (char c : S) {
-        auto advance = waiting[c];
-        waiting[c].clear();
-        for (auto it : advance)
-            waiting[*++it].push_back(it);
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int count=0,k=s.size();
+      unordered_map<string,int>mymap;
+      for(int i=0;i<words.size();i++)
+        mymap[words[i]]++;
+      for(auto x:mymap)
+      {
+        int start1=0,start2=0;
+        string p=x.first;
+        int m=p.size();
+        while(start1<k&&start2<m)
+        {
+          if(p[start2]==s[start1])
+            start1++,start2++;
+          else
+            start1++;
+        }if(start2==m)
+          count+=x.second;
+      }return count;
     }
-    return waiting[0].size();
-}
 };
