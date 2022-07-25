c6d2a702a40ab6ea21004c43f3afab3bfbbb458d
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-07-25 23:38:06 +0530

    Time: 10 ms (73.39%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,58 +1,40 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size()-1;
-        vector<int>ans(2,0);
-        ans[0]=-1;
-        ans[1]=-1;
-        while(low<=high){
-            int mid=low+high;
-            mid=mid/2;
-            if(nums[mid]>target){
-                cout<<mid<<endl;
-                high=mid-1;
+    int low=0;
+    int high=nums.size()-1;
+        vector<int> ans;
+        int a=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                a=mid;
+               high=mid-1;
             }
-            else if(nums[mid]<target){
-                cout<<mid<<endl;
+            else if(nums[mid]<target)
                 low=mid+1;
-            }
-            else{
-                cout<<mid<<endl;
-                ans[0]=mid;
+            else
                 high=mid-1;
-            }
         }
-        low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low+high;
-            mid=mid/2;
-            if(nums[mid]>target){
-                // cout<<mid<<endl;
+     low=0;
+     high=nums.size()-1;
+        int b=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                b=mid;
+               low=mid+1;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
                 high=mid-1;
-            }
-            else if(nums[mid]<target){
-                // cout<<mid<<endl;
-                low=mid+1;
-            }
-            else{
-                // cout<<mid<<endl;
-                ans[1]=mid;
-                low=mid+1;
-            }
-        }
-        return ans;
+        }   
         
+        return {a,b};
     }
 };
-// 5 8 8 8 8 10
-// 0 1 2 3 4 5
-    
-// low=7;
-// high=10;
-
-// low=8;
-// high=10;
-
-
-
