4810b308371e5cd655f2ae7a945e59969b14d122
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-07-28 00:29:05 +0530

    Time: 0 ms (100.00%), Space: 12.7 MB (78.06%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,30 +1,48 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         if(root==NULL)
+//             return ;
+//         TreeNode* left1=root->left;
+//         TreeNode* right1=root->right;
+//         root->left=NULL;
+//         flatten(left1);
+//         flatten(right1);
+//         root->right=left1;
+//         TreeNode* curr=root;
+        
+//         while(curr->right!=NULL)
+//             curr=curr->right;
+        
+//         curr->right=right1;
+//     }
+// };
 class Solution {
-public:
-    void flatten(TreeNode* root) {
+public:    
+    void help(TreeNode *root)
+    {
         if(root==NULL)
-            return ;
-        TreeNode* left1=root->left;
-        TreeNode* right1=root->right;
+            return;
+        TreeNode*left_pointer=root->left;
+        TreeNode*right_pointer=root->right;
+        
+        help(root->left);
+        help(root->right);
+        
+        if(left_pointer!=NULL)
+        {
+                root->right=left_pointer;
+                TreeNode* curr=root;
+                while(curr->right!=NULL){
+                    curr=curr->right;
+                }
+            curr->right=right_pointer;
+            
+        }
         root->left=NULL;
-        flatten(left1);
-        flatten(right1);
-        root->right=left1;
-        TreeNode* curr=root;
-        
-        while(curr->right!=NULL)
-            curr=curr->right;
-        
-        curr->right=right1;
+        return;
+    }
+    void flatten(TreeNode* root) {
+        help(root);
     }
 };
