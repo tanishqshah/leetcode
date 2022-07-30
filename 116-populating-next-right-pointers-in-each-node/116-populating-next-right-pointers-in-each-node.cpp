17338a8aee615ba8d75ffd0a0ffcf65bbece46c4
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-07-31 00:38:34 +0530

    Time: 31 ms (52.19%), Space: 16.7 MB (96.76%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -1,15 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-Node* connect(Node* root) {
-    //base case
-    if(root == NULL) return NULL;
-    //connects the left subtree of same level with right subtree of that same level 
-    if(root->left != NULL) root->left->next = root->right;
-    //connect the rightmost node of a level to the leftmost node of the next level.
-    if(root->right != NULL && root->next != NULL) root->right->next = root->next->left;
-    //recursive calls for left and right subtrees.
-    connect(root->left);
-    connect(root->right);
-    return root;
-   }
+    Node* connect(Node* root) {
+        if(root==NULL)
+            return NULL;
+        if(root->left!=NULL)
+            root->left->next=root->right;
+        if(root->right!=NULL && root->next!=NULL)
+            root->right->next=root->next->left;
+        connect(root->left);
+        connect(root->right);
+        return root;
+    }
 };
