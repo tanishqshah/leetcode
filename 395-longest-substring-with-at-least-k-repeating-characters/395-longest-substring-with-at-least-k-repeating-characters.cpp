e8c2c71cb97e22297b52d3fe20ea5e7276813d55
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-08-02 22:42:57 +0530

    Time: 4 ms (75.10%), Space: 6.8 MB (62.67%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        int countMap[26];
-        int maxUnique = getMaxUniqueLetters(s);
-        int result = 0;
-        for (int currUnique = 1; currUnique <= maxUnique; currUnique++) {
-            // reset countMap
-            memset(countMap, 0, sizeof(countMap));
-            int windowStart = 0, windowEnd = 0, idx = 0, unique = 0, countAtLeastK = 0;
-            while (windowEnd < s.size()) {
-                // expand the sliding window
-                if (unique <= currUnique) {
-                    idx = s[windowEnd] - 'a';
-                    if (countMap[idx] == 0) unique++;
-                    countMap[idx]++;
-                    if (countMap[idx] == k) countAtLeastK++;
-                    windowEnd++;
-                }
-                // shrink the sliding window
-                else {
-                    idx = s[windowStart] - 'a';
-                    if (countMap[idx] == k) countAtLeastK--;
-                    countMap[idx]--;
-                    if (countMap[idx] == 0) unique--;
-                    windowStart++;
-                }
-                if (unique == currUnique && unique == countAtLeastK)
-                    result = max(windowEnd - windowStart, result);
-            }
+        int n=s.length();
+        if(n==0 || n<k)
+            return 0;
+        if(k<1)
+            return n;
+        unordered_map<char,int>count;
+        for(char c : s){
+            count[c]++;
         }
-
-        return result;
-    }
-
-    // get the maximum number of unique letters in the string s
-    int getMaxUniqueLetters(string s) {
-        bool map[26] = {0};
-        int maxUnique = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (!map[s[i] - 'a']) {
-                maxUnique++;
-                map[s[i] - 'a'] = true;
-            }
-        }
-        return maxUnique;
+        
+        int l=0;
+        while(l<n && count[s[l]]>=k)
+            l++;
+        if(l>=n-1)
+            return l;
+        
+        int ls1=longestSubstring(s.substr(0,l),k);
+        while(l<n && count[s[l]]<k)
+            l++;
+        int ls2=(l<n) ? longestSubstring(s.substr(l),k) : 0;
+        
+        return max(ls1,ls2);
+        
     }
 };
