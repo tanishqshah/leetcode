4ca6e38983da4a1ac707996d06bb839f9c7dd87c
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-08-02 15:14:02 +0530

    Time: 11 ms (89.21%), Space: 9.1 MB (77.83%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        // int dp[nums.size()+1][target+1];
         target=abs(target);
-        int n=nums.size();
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum=sum+nums[i];
         }
-        int m=(sum+target)/2;
+        // sum=sum-target;
         if(sum<target || (sum+target)%2!=0)
             return 0;
         
-        return count(nums,n,m);
+        int m=sum+target;
+        m=m/2;
         
-    }
-    int count(vector<int>&nums , int n ,int m){
+        int n=nums.size();
+        
         int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0){
-                    dp[i][j]=0;
-                }
-                if(j==0){
-                    dp[i][j]=1;
-                }
-            }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
         }
-        
-        for(int i=1 ;i<=n;i++){
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
             for(int j=0;j<=m;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
+                if(j>=nums[i-1]){
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
                 }
-                else{
+                else
                     dp[i][j]=dp[i-1][j];
-                }
             }
         }
         return dp[n][m];
