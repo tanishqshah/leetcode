9cb6f932a4cfe57c03236153d3b21c7a8c7b48ed
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-08-05 19:12:10 +0530

    Time: 20 ms (72.93%), Space: 13.3 MB (18.84%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,60 +1,52 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-        int n=s1.size(); 
-        int m=s2.size();
-        int t[n+1][m+1]; 
-        string res;      
-    for(int i=0;i<=n;i++)
-    {
-        for(int j=0;j<=m;j++)
-        {
-            if(i==0||j==0)
-                t[i][j]=0;
-            else if(s1[i-1]==s2[j-1])
-            {
-                t[i][j]=1+t[i-1][j-1];
-            }
-            else
-            {
-                t[i][j]=max(t[i-1][j],t[i][j-1]);
+    string shortestCommonSupersequence(string s, string t) {
+        int n1=s.length();
+        int n2=t.length();
+        int dp[n1+1][n2+1];
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                    dp[0][0]=1;
+                    continue;
+                }
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-    }     
-       //print lcs
-    int i=n,j=m;
-    while(i>0 && j>0)
-    {
-        if(s1[i-1]==s2[j-1])
-        {
-            res.push_back(s1[i-1]);
-            i--;
-            j--;
-        }
-        else
-        {
-            if(t[i-1][j]>t[i][j-1])
-            {
-                res.push_back(s1[i-1]);
+        string ans="";
+        int i=n1;
+        int j=n2;
+        while(i>0 && j>0){
+            if(s[i-1]==t[j-1]){
+                ans=s[i-1]+ans;
                 i--;
-            } else{
-                res.push_back(s2[j-1]);
                 j--;
             }
+            else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    ans=s[i-1]+ans;
+                    i--;
+                }
+                else{
+                    ans=t[j-1]+ans;
+                    j--;
+                }
+            }
         }
-    }
-    while(i>0)
-    {
-        res.push_back(s1[i-1]);
-        i--;
-    }
-    while(j>0) 
-    {
-        res.push_back(s2[j-1]);
-        j--;
-        
-    }
-        reverse(res.begin(),res.end()); 
-        return res;
+        while(i>0){
+            ans=s[i-1]+ans;
+            i--;
+        }
+        while(j>0){
+            ans=t[j-1]+ans;
+            j--;
+        }
+        return ans;
     }
 };
