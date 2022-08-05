7173db30768c56295e4f219a92d445f32dba5ea1
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-08-05 12:53:20 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -3,11 +3,12 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         vector<unsigned int>dp(target+1,0);
         dp[0]=1;
-        // sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         for(int i=1;i<=target;i++){
             for(auto j : nums){
-                if(i>=j)
-                    dp[i]=dp[i]+dp[i-j];
+                if(i<j)
+                    break;
+                dp[i]=dp[i]+dp[i-j];
             }
         }
         return dp[target];
