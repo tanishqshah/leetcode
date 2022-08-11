b7dc698a06b8d3656c3ba14ba446051d4b8908ed
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-08-11 10:35:30 +0530

    Time: 127 ms (48.27%), Space: 40.4 MB (59.76%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        if(arr.size()==1)
+            if(arr.size()==1)
             return 1;
         int ans=1;
         bool flag=arr[0]<arr[1];
@@ -10,11 +10,11 @@ public:
         for(int i=1;i<arr.size()-1;i++){
             if(flag && arr[i]>arr[i+1] || !flag && arr[i]<arr[i+1]){
                 count++;
-                ans=max(ans,count);
             }
             else {
                 count=(arr[i]!=arr[i+1]) ? 2 : 1;
             }
+            ans=max(ans,count);
             flag=arr[i]<arr[i+1];
         }
         return ans;
