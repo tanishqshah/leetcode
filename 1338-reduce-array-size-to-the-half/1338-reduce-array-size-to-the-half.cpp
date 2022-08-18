68b7965e8c44c77f167f9fc54ccd2b1129106ed3
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-08-19 00:32:26 +0530

    Time: 260 ms (79.20%), Space: 78.3 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int minSetSize(vector<int>& nums) {
-        int n=nums.size();
+    int minSetSize(vector<int>& arr) {
         unordered_map<int,int>mp;
-        
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
         }
         priority_queue<int>pq;
-        
         for(auto i : mp){
             pq.push(i.second);
         }
         int ans=0;
-        int res=0;
-        while(ans*2<nums.size()){
+        int count=0;
+        int n=arr.size();
+        while(ans*2<arr.size()){
             ans=ans+pq.top();
             pq.pop();
-            res++;
+            count++;
         }
-        return res;
-        
-        
+        return count;
     }
 };
