694637670466112d6110ac6229f4971625b2ae12
tanishqshah <68581432+tanishqshah@users.noreply.github.com>
2022-09-04 20:59:52 +0530

    Time: 3 ms (93.93%), Space: 12.3 MB (82.65%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,36 +1,43 @@
-
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,vector<int>>>nodes;
+        map<int,map<int,vector<int>>>mp;
         queue<pair<TreeNode* , pair<int,int>>>q;
         vector<vector<int>>ans;
-        
         if(root==NULL)
             return ans;
-        
         q.push(make_pair(root,make_pair(0,0)));
         while(!q.empty()){
-            pair<TreeNode*,pair<int,int>>temp = q.front();
+            pair<TreeNode* , pair<int,int>>top=q.front();
             q.pop();
-            TreeNode* frontNode=temp.first; 
-            int hd=temp.second.first;
-            int lvl=temp.second.second;
-            
-            nodes[hd][lvl].push_back(frontNode->val);
-            
-            if(frontNode->left)
-                q.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
-             if(frontNode->right)
-                q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+            TreeNode* front=top.first;
+            int hd=top.second.first;
+            int lvl=top.second.second;
+            mp[hd][lvl].push_back(front->val);
+            if(front->left){
+                q.push(make_pair(front->left,make_pair(hd-1,lvl+1)));
+            }
+            if(front->right){
+                q.push(make_pair(front->right,make_pair(hd+1,lvl+1)));
+            }
         }
-        for(auto i : nodes){
+        for(auto i : mp){
             vector<int>col;
-            for(auto q : i.second){
-                sort(q.second.begin(),q.second.end());
-                for(int k=0;k<q.second.size();k++)
-                    col.push_back(q.second[k]);
+            for(auto j : i.second){
+                sort(j.second.begin(),j.second.end());
+                for(int k=0;k<j.second.size();k++)
+                    col.push_back(j.second[k]);
             }
             ans.push_back(col);
         }
