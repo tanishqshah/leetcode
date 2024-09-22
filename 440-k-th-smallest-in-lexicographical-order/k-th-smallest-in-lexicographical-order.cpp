f5a7d7b8c3974d01f685ff5d878fec97b7cdba2f
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2024-09-22 21:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (84.46%) - LeetSync

---
+++ 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        int currentPrefix = 1;
+        --k;  // Decrement k to handle zero-based indexing
+        
+        while (k > 0) {
+            int count = countNumbersWithPrefix(currentPrefix, n);
+            if (k >= count) {
+                ++currentPrefix;  // Move to the next prefix
+                k -= count;
+            } else {
+                currentPrefix *= 10;  // Go deeper in the current prefix
+                --k;
+            }
+        }
+        
+        return currentPrefix;
+    }
+
+private:
+    int countNumbersWithPrefix(int prefix, int n) {
+        long long firstNumber = prefix, nextNumber = prefix + 1;
+        int totalCount = 0;
+
+        while (firstNumber <= n) {
+            totalCount += static_cast<int>(min(n + 1LL, nextNumber) - firstNumber);
+            firstNumber *= 10;
+            nextNumber *= 10;
+        }
+
+        return totalCount;
+    }
+};
