2cb01143068dc2a9b76434ba8b3522afa92a6ea7
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2024-09-28 14:42:03 +0530

    Time: 47 ms (84.77%) | Memory: 45.6 MB (64.51%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.java
@@ -0,0 +1,56 @@
+class Event
+{
+  int start;
+  int end;
+  public Event(int start, int end)
+  {
+    this.start = start;
+    this.end = end;
+  }  
+}
+
+class MyCalendarTwo {
+    List<Event> overLapBookings;
+    List<Event> bookings;
+
+    public MyCalendarTwo() {
+        overLapBookings = new ArrayList<>();
+        bookings = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        
+    for(Event e : overLapBookings)
+    {
+      if(isOverlap(e, start, end))
+      return false;  
+    } 
+
+    for(Event e : bookings)
+    {
+      if(isOverlap(e, start, end))
+      {
+        overLapBookings.add(getOverlapEvent(e, start, end));
+      }    
+    }  
+       bookings.add(new Event(start, end));
+
+       return true; 
+    }
+
+    public boolean isOverlap(Event e, int start, int end)
+    {
+      return Math.max(e.start, start)<Math.min(e.end, end); 
+    }
+
+    public Event getOverlapEvent(Event e, int start, int end)
+    {
+      return new Event(Math.max(e.start, start), Math.min(e.end, end));  
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
