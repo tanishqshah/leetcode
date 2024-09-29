8f3a0a5a63575f55d456f32d9db4bc25bbe962e7
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2024-09-29 22:22:05 +0530

    Time: 85 ms (85.09%) | Memory: 50.8 MB (90.87%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.cpp
+++ 432-all-oone-data-structure/all-oone-data-structure.cpp
@@ -1,40 +1,122 @@
-// Way - 01 : Complete Brute Force
+// Way - 03 : Optimized Doubly LL
+struct Node {
+    string word;
+    int freq;
+    Node* prev;
+    Node* next;
+
+    Node(string k) : word(k), freq(1), prev(nullptr), next(nullptr) {}
+};
+
 class AllOne {
 public:
-    unordered_map<string, int> freq;
+    Node *head, *tail;
+    unordered_map<string, Node*> um;
+
+    // for base connection join head and tail [ both are empty ]
     AllOne() {
-       // nothing to do here
+        head = new Node("");
+        tail = new Node("");
+        head->next = tail;
+        tail->prev = head;
     }
-    
-    void inc(string key) {
-        freq[key]++;
-    }
-    
-    void dec(string key) {
-        freq[key]--;
-    }
-    
-    string getMaxKey() {
-        int mx = 0;
-        string ans = "";
-        for(auto& val:freq){
-            if(val.second > mx){
-                mx = val.second;
-                ans = val.first;
-            }
+
+    // moving character to new node if its count increase
+    void moveToCorrectNextPosition(Node* node) {
+        Node* ptr = node->next;
+        // checkig if any node exist with current frequency
+        while (ptr != tail && node->freq > ptr->freq) {
+            ptr = ptr->next;
         }
+
+        if (ptr != node->next) {
+            // remove node from current place
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+
+            // add it to new place before ptr
+            ptr->prev->next = node;
+            node->prev = ptr->prev;
+            node->next = ptr;
+            ptr->prev = node;
+        }
+    }
+
+    // moving character to new node as its frequency decreased
+    void moveToCorrectPrevPosition(Node* node) {
+        Node* ptr = node->prev;
+        // checkig if any node exist with current frequency
+        while (ptr != head && node->freq < ptr->freq) {
+            ptr = ptr->prev;
+        }
+
+        if (ptr != node->prev) {
+            // remove node from current place
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+
+            // add it to new place before ptr
+            ptr->next->prev = node;
+            node->next = ptr->next;
+            node->prev = ptr;
+            ptr->next = node;
+        }
+    }
+
+    // increase the count of word
+    void inc(string word) {
+        if (um.find(word) != um.end()) {
+            // already present -> update its count
+            Node* node = um[word];
+            node->freq++;
+
+            // fix node position after upgrade
+            moveToCorrectNextPosition(node);
+        } 
+        else {
+            // create a new node
+            Node* node = new Node(word);
+            node->next = head->next;
+            node->prev = head;
+
+            head->next->prev = node;
+            head->next = node;
+
+            // store it in map
+            um[word] = node;
+
+            // insert it into the list
+            moveToCorrectNextPosition(node);
+        }
+    }
+
+    void dec(string word) {
+        Node* node = um[word];
+        node->freq--;
+
+        moveToCorrectPrevPosition(node);
+
+        // Here node will be at first position just next to head
+        // if freq of node is zero then we follow same logic and 
+        if (node->freq == 0) {
+            node->next->prev = node->prev;
+            node->prev->next = node->next;
+            um.erase(word);
+            delete node;
+        }
+    }
+
+    string getMaxKey() {
+        string ans = "";
+        if (tail->prev != head)
+            ans = tail->prev->word;
         return ans;
     }
-    
+
     string getMinKey() {
-        int mini = INT_MAX;
         string ans = "";
-        for(auto& val:freq){
-            if(val.second!=0 and val.second < mini){
-                mini = val.second;
-                ans = val.first;
-            }
-        }
+        if (head->next != tail)
+            ans = head->next->word;
         return ans;
     }
 };
