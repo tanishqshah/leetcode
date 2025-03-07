92e62897a8a34a0686081ad00c2d01928dd3c8bc
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2025-03-07 12:11:08 +0530

    Time: 2 ms (82.46%) | Memory: 13.9 MB (99.16%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,18 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
+    // int lowerbound()
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp(nums.size(),1);
-        if(nums.size()==1)
-            return 1;
-        int answer=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && dp[i]<dp[j]+1)
-                    dp[i]=dp[j]+1;
-                answer=max(answer,dp[i]);
+        vector<int>tempArray;
+        tempArray.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>tempArray.back())
+                tempArray.push_back(nums[i]);
+            else{
+                auto index = lower_bound(tempArray.begin(),tempArray.end(),nums[i]);
+                *index= nums[i];
             }
         }
-        return answer;
+        return tempArray.size();
     }
 };
