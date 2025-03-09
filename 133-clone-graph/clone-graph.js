fdcee587cea9bfafc2b0794874f0e01cf2eb5cc0
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2025-03-09 21:56:19 +0530

    Time: 59 ms (48.87%) | Memory: 56.1 MB (29.98%) - LeetSync

---
+++ 133-clone-graph/clone-graph.js
@@ -0,0 +1,15 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(node) {
+    return structuredClone(node);
+};
