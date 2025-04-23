c911ed96bfe1074c11c4352f73319963f11e7cc6
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2025-04-23 15:50:19 +0530

    Time: 42 ms (14.16%) | Memory: 44.2 MB (32.00%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
-        if (j == t.size()) return 1;       // All characters in t matched
-        if (i == s.size()) return 0;       // s exhausted, t not matched
+    int count=0;
+    int solve(string &s,string &t,int i,int j,vector<vector<long long>>&dp){
+        if(j==t.length()) return 1;
+        if(i==s.length()) return 0;
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int res = 0;
-        if (s[i] == t[j]) {
-            res += solve(i + 1, j + 1, s, t, dp); // Match
+        if(dp[i][j]!=-1)  return dp[i][j];
+        int ways=0;
+        if(s[i]==t[j]){
+            ways+=solve(s,t,i+1,j+1,dp);
         }
-        res += solve(i + 1, j, s, t, dp);         // Skip
-
-        return dp[i][j] = res;
+        ways+=solve(s,t,i+1,j,dp);
+        return dp[i][j]=ways;
     }
-
     int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(0, 0, s, t, dp);
+        vector<vector<long long>>dp(s.length(),vector<long long>(t.length(),-1));
+        return solve(s,t,0,0,dp);
+        // return count;
+
     }
 };
