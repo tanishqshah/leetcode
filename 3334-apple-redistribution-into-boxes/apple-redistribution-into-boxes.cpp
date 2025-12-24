abc956090b76bfe9cdddba37a952d7f0adde0cf0
Tanishq shah <tanishq.shah2019@vitstudent.ac.in>
2025-12-24 11:36:40 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (45.84%) - LeetSync

---
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int count=0;
+        int sumApple=0;
+        sort(capacity.begin(),capacity.end());
+        for(int i=0;i<apple.size();i++){
+            sumApple += apple[i];
+        }
+        for(int i=capacity.size()-1;i>=0;i--){
+            if(sumApple>0){
+                count++;
+            }
+            sumApple=sumApple-capacity[i];
+        }
+        return count;
+    }
+};
